af781218f74dac4158138f1328d3012ec4169848
MD IMRAN MRIDHA <mdimranmridha.pro@gmail.com>
2022-09-14 22:46:14 +0600

    error count fixed

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ const validate = (key) => {
   if (key === questionText[userText.length - 1]) {
     return true;
   }
+  errorCount++;
   return false;
 };
 
@@ -80,7 +81,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold space">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -100,6 +101,7 @@ const closeModal = () => {
 };
 
 const start = () => {
+  
   // If already started, do not start again
   if (startTime) return;
 
@@ -113,14 +115,15 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
+    
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
     count--;
-  }, 1000);
+  }, 100);
 };
 
 // START Countdown
@@ -135,5 +138,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
