14d05e8bc70550754cf71cc7eda2182b57dc5879
MD IMRAN MRIDHA <mdimranmridha.pro@gmail.com>
2022-09-15 01:35:48 +0600

    Header Added

--- history.js
+++ history.js
@@ -8,7 +8,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   <h3>${questionText}</h3>
   <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-  <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+  <p>You made <span class="bold red space">${errorCount}</span> mistakes</p>
   </div>
   `;
 
@@ -26,12 +26,15 @@ function displayHistory() {
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
 
   previousTests.forEach((test) => {
+    console.log(test)
+    const {timeTaken} = test;
+    const timeTakenParse = parseInt(timeTaken);
     const newRow = document.createElement("div");
     newRow.classList.add("card");
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${timeTakenParse}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
